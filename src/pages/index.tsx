bff3a59fcba7f2cf21694d80b792442a39f3e30e
Wataru Manji <manji@linux.com>
2021-08-05 18:38:19 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm manji0<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">Wataru Manji</span>
+          <h2>I am Tech Lead/Manager of Verda Reliability Engineering Team, LINE Corp.<br>And an expert at system design in microservice architecture and data flow design such as monitoring.</h2>
           <SocialList />
         </div>
       </div>
