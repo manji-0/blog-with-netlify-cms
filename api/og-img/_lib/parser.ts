9677af5d9911fb461d1058e43d9a98a9177ba197
Wataru Manji <manji@linux.com>
2021-08-08 17:15:05 +0900

    add og-img function

---
+++ api/og-img/_lib/parser.ts
@@ -0,0 +1,42 @@
+import { IncomingMessage } from 'http';
+import { parse } from 'url';
+import { Theme, ParsedRequest } from './types';
+
+export function parseRequest(req: IncomingMessage) {
+    console.log('HTTP ' + req.url);
+    const { pathname, query } = parse(req.url || '/', true);
+    const { fontSize, theme, md, background } = (query || {});
+
+    if (Array.isArray(fontSize)) {
+        throw new Error('Expected a single fontSize');
+    }
+    if (Array.isArray(theme)) {
+        throw new Error('Expected a single theme');
+    }
+    if (Array.isArray(background)) {
+        throw new Error('Expected a single background');
+    }
+    
+    const arr = (pathname || '/').slice(1).split('.');
+    let extension = '';
+    let text = '';
+    if (arr.length === 0) {
+        text = '';
+    } else if (arr.length === 1) {
+        text = arr[0];
+    } else {
+        extension = arr.pop() as string;
+        text = arr.join('.');
+    }
+
+    const parsedRequest: ParsedRequest = {
+        fileType: extension === 'jpeg' ? extension : 'png',
+        text: decodeURIComponent(text),
+        theme: ['black', 'white', 'stroke'].includes(theme) ? theme as Theme : 'black',
+        md: md === '1' || md === 'true',
+        fontSize: fontSize || '96px',
+        background: decodeURIComponent(background)
+    };
+    return parsedRequest;
+}
+
---
+++ api/og-img/_lib/types.ts
@@ -0,0 +1,12 @@
+export type FileType = 'png' | 'jpeg';
+export type Theme = 'black' | 'white' | 'stroke';
+
+export interface ParsedRequest {
+    fileType: FileType;
+    text: string;
+    theme: Theme;
+    md: boolean;
+    fontSize: string;
+    background?: string;
+}
+
